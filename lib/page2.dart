ec7ac7d26f2d4586bb5de30066f62c6e2242499d
foorcun <msnifoorcun@hotmail.com>
2021-03-30 23:19:13 +0300

    3:17 prevent back button press on Android

--- lib/page2.dart
+++ lib/page2.dart
@@ -3,16 +3,27 @@ import 'package:flutter/material.dart';
 class Page2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pop(context, true);
-        },
-      ),
-      body: Container(
-        child: Center(
-          child: Text('Page 2',
-              style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
+    return WillPopScope(
+      onWillPop: () => Future.value(false),
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.pop(context, true);
+              }),
+          title: Text("Page 2"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.pop(context, true);
+          },
+        ),
+        body: Container(
+          child: Center(
+            child: Text('Page 2',
+                style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
+          ),
         ),
       ),
     );
